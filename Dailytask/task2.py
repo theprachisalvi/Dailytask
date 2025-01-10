b955171da58fd3234dd5529ce7e713f07416d377
theprachisalvi <144591654+theprachisalvi@users.noreply.github.com>
2025-01-10 10:41:19 +0530

    Delete Dailytask/task2.py

--- Dailytask/task2.py
+++
@@ -1,6 +0,0 @@
-#Declare two variables and print that which variable is largest using ternary operators
-
-a = int(input("Enter the first number: "))
-b = int(input("Enter the second number: "))
-largest = a if a > b else b
-print("The largest number is: ",largest)
