c1ddd2786fa238a7f802767bacea59ddef56ba7e
theprachisalvi <144591654+theprachisalvi@users.noreply.github.com>
2025-01-10 10:41:50 +0530

    Delete Dailytask/task3.py

--- Dailytask/task3.py
+++
@@ -1,5 +0,0 @@
-#Python program to convert the temperature in degree centigrade to Fahrenheit
-
-celsius = float(input("Enter temperature in Celsius: "))
-fahrenheit = (celsius * 9/5) + 32
-print(celsius,"degree is equal to ",fahrenheit,"degreeF")
