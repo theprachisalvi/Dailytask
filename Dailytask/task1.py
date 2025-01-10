c110eeaf19ac0bee8485a7c0cff2b51dc659cdee
theprachisalvi <144591654+theprachisalvi@users.noreply.github.com>
2025-01-10 10:41:09 +0530

    Delete Dailytask/task1.py

--- Dailytask/task1.py
+++
@@ -1,7 +0,0 @@
-#Calculate the multiplication and sum of two numbers
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-multiplication = num1 * num2
-sum = num1 + num2
-print("The multiplication of two numbers is: ", multiplication)
-print("The sum of two numbers is: ",sum)
